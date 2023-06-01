ac2c938db5e0d519ac1e3674a33f5ee3ec30931b
lambiv-dzenyuy <gillslambiv@gmail.com>
2023-06-01 22:18:17 +0100

    feat: add article page

--- pages/login.js
+++ pages/login.js
@@ -4,14 +4,9 @@ import * as crypto from "crypto";
 import styles from "../styles/RegisterAndLogin.module.css";
 
 export default function Login() {
+  const router = useRouter();
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [isLogin, setIsLogin] = useState(false);
-
-  function clearToken() {
-    window.localStorage.setItem("token", "");
-    setIsLogin(false);
-  }
 
   function validateUser(event) {
     event.preventDefault();
@@ -25,53 +20,38 @@ export default function Login() {
       user.email === email && user.password === hashedPassword;
 
     if (checkUserInputDataWithLocalStorageUserData) {
-      const token = crypto.createHash("sha256").update(password).digest("hex");
+      const token = crypto.createHash("sha256").update(password+email).digest("hex");
       window.localStorage.setItem("token", token);
-      setIsLogin(true);
+      router.push("/article");
     } else window.alert("Email or password is incorrect");
   }
+  return (
+    <div className={styles.main_container}>
+      <div className={styles.form_container}>
+        <div className={styles.form_header}>Login</div>
+        <form onSubmit={validateUser}>
+          <div className={styles.fields_container} style={{ gap: "16px" }}>
+            <input
+              type="email"
+              value={email}
+              className={styles.form_field}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              value={password}
+              className={styles.form_field}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
 
-  if (isLogin) {
-    return (
-      <div className={styles.main_container}>
-        <button
-          onClick={clearToken}
-          style={{
-            padding: "16px",
-          }}
-        >
-          Logout
-        </button>
+            <button type="submit" className={styles.btn_submit}>
+              Login
+            </button>
+          </div>
+        </form>
       </div>
-    );
-  } else
-    return (
-      <div className={styles.main_container}>
-        <div className={styles.form_container}>
-          <div className={styles.form_header}>Login</div>
-          <form onSubmit={validateUser}>
-            <div className={styles.fields_container} style={{ gap: "16px" }}>
-              <input
-                type="email"
-                value={email}
-                className={styles.form_field}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-              />
-              <input
-                type="password"
-                value={password}
-                className={styles.form_field}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-              />
-
-              <button type="submit" className={styles.btn_submit}>
-                Login
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
+    </div>
+  );
 }
